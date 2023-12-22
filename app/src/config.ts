bc78952b5a246d93e7503ebb461b68e4576b89b3
Thomas Gautvedt <thomas.gautvedt@gmail.com>
2023-12-22 21:57:04 +0100

    fix: change config for 2024

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,6 +1,6 @@
 // Format YYYY-MM-DD
-export const CHALLENGE_DATE_START = "2022-01-01";
-export const CHALLENGE_DATE_END = "2022-06-30";
+export const CHALLENGE_DATE_START = "2024-01-01";
+export const CHALLENGE_DATE_END = "2024-06-30";
 
 export const TARGET_WORKOUTS = 90;
 export const DONATION_EACH = 500;
